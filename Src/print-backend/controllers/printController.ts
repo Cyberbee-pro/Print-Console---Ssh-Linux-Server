5a8deb3b4dd040049b90e2bb41443707984ba224
Cyberbee-pro <cyberbeepro@gmail.com>
2026-07-10 02:06:21 +0530

    backend new things

--- Src/print-backend/controllers/printController.ts
+++ Src/print-backend/controllers/printController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import type { Request, Response } from "express";
 import { readdir, unlink } from "node:fs/promises";
 import path from "node:path";
 import { spawn } from "node:child_process";
--- Src/print-backend/index.ts
+++ Src/print-backend/index.ts
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Initialize global server filters
-app.use(cors());
+app.use(cors({origin : process.env.Frontend_Origin}));
 app.use(express.json());
 
 // Mount the modular routes tree at the root prefix
